c034e4ba734c191f70960e5a282c0ca3eb4eab19
rutujayadav26 <152773545+rutujayadav26@users.noreply.github.com>
2024-01-15 17:30:25 +0530

    Add files via upload
    I have developed this basic BMI calculator from Task 2.This task is assigned by OASIS INFOBYTE  as an internship I have developed this project using python programming language.

---
+++ BMI CALCULATOR.py
@@ -0,0 +1,34 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      rutuj
+#
+# Created:     15-01-2024
+# Copyright:   (c) rutuj 2024
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def calculate_bmi(weight, height):
+    return weight / (height ** 2)
+
+def classify_bmi(bmi):
+    if bmi < 18.5:
+        return "Underweight"
+    elif 18.5 <= bmi < 25:
+        return "Normal"
+    elif 25 <= bmi < 30:
+        return "Overweight"
+    else:
+        return "Obesity"
+
+def main():
+    weight,height=map(float,input("Enter your weight in kilograms and height in meaters: ").split())
+    bmi = calculate_bmi(weight, height)
+    category = classify_bmi(bmi)
+
+    print(f"Your BMI is {bmi:.2f}.")
+    print(f"You are classified as {category}.")
+
+
+calculate=main()
